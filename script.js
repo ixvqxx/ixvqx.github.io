51c973aea1b9f822991a7ca38a1e58a870fddf70
ixvqx <118126157+ixvqxx@users.noreply.github.com>
2024-03-02 23:46:09 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("joinButton").addEventListener("click", function() {
+  // Replace this with your Discord invite link
+  window.location.href = "YOUR_DISCORD_INVITE_LINK";
+});
