29172a894121bf44eadb4380bfab8a55368f9db1
ixvqx <118126157+ixvqxx@users.noreply.github.com>
2024-03-21 11:28:20 +0100

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("joinButton").addEventListener("click", function() {
-  // Replace this with your Discord invite link
-  window.location.href = "YOUR_DISCORD_INVITE_LINK";
-});
